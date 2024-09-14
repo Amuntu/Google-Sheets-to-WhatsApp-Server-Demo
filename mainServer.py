7bf44623583bef595fd32780aec01e9e46ec527a
Amin Haydar <122865043+Amuntu@users.noreply.github.com>
2024-09-14 19:44:17 +0300

    Update mainServer.py

--- mainServer.py
+++ mainServer.py
@@ -2,7 +2,6 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 # Google Sheets setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Users/ASUS/Downloads/cre.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('/path/to/your/sheetCredentials.json', scope)
 client = gspread.authorize(creds)
-sheet = client.open("Flutter GSheets").sheet1
-print(dir(sheet))
+sheet = client.open("Flutter GSheets").sheet1 #"Flutter GSheets" : Replace it with your our Spreed-Sheet Name.
